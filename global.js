49c9ed18a8d8d03560b87ec0f7f623ba2dc4361d
v1zhu <vzhu10547@gmail.com>
2026-04-17 18:33:40 -0700

    Add javascript

---
+++ global.js
@@ -0,0 +1,80 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"
+  : "/portfolio/";
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume'},
+    { url: 'contact/', title: 'Contact'},
+    { url: 'https://github.com/v1zhu', title: 'Profile'}
+];
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `<label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>`,
+);
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+    
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname,);
+    if (a.host !== location.host) {
+        a.target = '_blank';}
+
+
+  nav.append(a);      
+}
+let select = document.querySelector('.color-scheme select');
+
+if ('colorScheme' in localStorage) {
+  document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+  select.value = localStorage.colorScheme;
+}
+
+select.addEventListener('input', function(event) {
+  console.log('color scheme changed to', event.target.value);
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value;
+});
+
+let form = document.querySelector('form');
+
+form?.addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  let data = new FormData(form);
+  let url = form.action + '?';
+  
+  for (let [name, value] of data) {
+    url += `${name}=${encodeURIComponent(value)}&`;
+  }
+  
+  url = url.slice(0, -1);
+  
+  location.href = url;
+});
+
