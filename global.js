61735e172a548f2593ca9daf44f1bee10efd54eb
v1zhu <vzhu10547@gmail.com>
2026-05-08 17:34:25 -0700

    lab 6 pre work

--- global.js
+++ global.js
@@ -12,6 +12,7 @@ let pages = [
     { url: 'projects/', title: 'Projects' },
     { url: 'resume/', title: 'Resume'},
     { url: 'contact/', title: 'Contact'},
+    { url: 'meta/', title: 'Meta'},
     { url: 'https://github.com/v1zhu', title: 'Profile'}
 ];
 
